5a988cc19a681cf7cc00ec22d99d2240d135a2e4
stelson <sthelmo@gmail.com>
2020-08-29 20:36:04 +0200

    axios, json placeholder

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
+
+class App extends Component {
+  state = {
+    hello: null
+  }
+
+  componentDidMount() {
+    this.asyncFunction();
+  }
+
+  asyncFunction = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(json => console.log(json))
+  }
+
+  render() {
+    return (
+    <div>
+      {this.state.hello
+      ? <div> {this.state.hello} </div>
+      : null}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
